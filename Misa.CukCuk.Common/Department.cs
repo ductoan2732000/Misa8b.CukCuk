1f0c27fdae3151d125f3a1ff9e2cc41de10e158e
18021290@vnu.edu.vn <18021290@vnu.edu.vn>
2021-02-19 11:23:08 +0700

    change 19/02/2021

---
+++ Misa.CukCuk.Common/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Misa.CukCuk.Common
+{
+    public class Department
+    {
+        public Guid DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ Misa8b.CukCuk.Web/Controllers/DepartmentsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Misa8b.CukCuk.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Misa8b.CukCuk.Web.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        IDepartmentBL _departmentBL;
+        public DepartmentsController(IDepartmentBL departmentBL)
+        {
+            _departmentBL = departmentBL;
+        }
+        // GET: api/<DepartmentsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _departmentBL.GetAllData();
+            switch (datas.MisaCode)
+            {
+                case Misa.CukCuk.Common.Enum.Enumarations.MisaCode.Success:
+                    return Ok(datas);
+                case Misa.CukCuk.Common.Enum.Enumarations.MisaCode.Validate:
+                    return Ok(datas);
+                case Misa.CukCuk.Common.Enum.Enumarations.MisaCode.Error:
+                    return Ok(datas);
+                case Misa.CukCuk.Common.Enum.Enumarations.MisaCode.Exception:
+                    return Ok(datas);
+                default:
+                    return NoContent();
+            }
+        }
+
+        // GET api/<DepartmentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DepartmentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DepartmentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DepartmentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
